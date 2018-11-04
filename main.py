8e03c8afb8761b0b0d2c8cc9f523bc76253f5ac1
Leland <lelandmball@gmail.com>
2018-11-04 00:48:23 -0400

    Code review by Leland

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import motors as m
 import actions as a
 import constants as c
 
+# Your code looks pretty good! 
+# Once you accomplish line-following using the top-hat sensor,
+# then move on to line-following until you detect a soda can in front
+# of your robot (using the ET sensor). Pick up the can with your bot.
+# After that, you'll be ready to start on camera code! -LMB
 def main():
     s.servoinit()
     a.grabCan()
